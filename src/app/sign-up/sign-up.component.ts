a03cce4b1fcaae49736ffab52f3f5c9e54294fdc
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-07-06 19:39:35 +0200

    sign-up basic

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Firestore, collection, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  isSignUp: boolean = false;
+
+  signUpForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  });
+
+  constructor(private firestore: Firestore) { }
+
+  ngOnInit(): void {
+  }
+
+  signUp() {
+
+  }
 
 }
