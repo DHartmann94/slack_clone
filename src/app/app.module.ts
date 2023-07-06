91a6ea0e79d71e5165dd4ae3ed2f7225cba08008
MaximilianScheel <scheel.developmen@google.com>
2023-07-06 16:13:18 +0200

    Generate user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponent } from './test/test.component';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment.prod';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environments } from 'src/environments/environment.prod';
+import { UserProfileCardComponent } from './user-profile-card/user-profile-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    UserProfileCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirebaseApp(() => initializeApp(environments.firebase)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
   ],
---
+++ src/app/user-profile-card/user-profile-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-card',
+  templateUrl: './user-profile-card.component.html',
+  styleUrls: ['./user-profile-card.component.scss']
+})
+export class UserProfileCardComponent {
+
+}
