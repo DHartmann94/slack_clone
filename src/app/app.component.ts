48b17d45abce8ea2d509e8600fd331d3c7a60b8d
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-07-10 14:36:58 +0200

    start-screen animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slack_clone';
+
+  hideStartscreen = false;
+
+  // ngOnInit(): void {
+  //   setTimeout(() => {
+  //       this.hideStartscreen = true;
+  //   }, 3000);
+  // }
 }
