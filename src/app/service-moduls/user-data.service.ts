dca83ce5914dbd78810416dc2aec0c2a0f6a3853
Adrian Herzberger <adrianherzberger94@gmail.com>
2023-07-10 14:29:11 +0200

    new

--- src/app/service-moduls/user-data.service.ts
+++ src/app/service-moduls/user-data.service.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { DocumentData, Firestore, QuerySnapshot, collection, getDocs, query } from '@angular/fire/firestore';
+import { Observable, from, map } from 'rxjs';
+
+export interface UserDataInterface {
+  id: string;
+  name: string;
+  email: string;
+  createdAt?: any;
+  status?: any;
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserDataService {
 
-  constructor() { }
+  users: UserDataInterface[] = [];
+
+  constructor(
+    public firestore: Firestore
+  ) {}
+
+  getUserData(): Observable<UserDataInterface[]> {
+    const userCollection = collection(this.firestore, 'users');
+    const q = query(userCollection);
+
+    return from(getDocs(q)).pipe(
+      map((querySnapshot: QuerySnapshot<DocumentData>) => {
+        const storedUserData: UserDataInterface[] = [];
+
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+          const { name, email } = data;
+          const user: UserDataInterface = {
+            id: doc.id,
+            name: name,
+            email: email
+          };
+          storedUserData.push(user);
+        });
+        this.users = storedUserData;
+        return storedUserData;
+      })
+    );
+  }
 }
