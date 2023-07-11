0b190cb25054e5e4d423e0acd33ca136eefeba06
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-07-11 19:17:17 +0200

    new components

---
+++ src/app/password-confirm/password-confirm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-confirm',
+  templateUrl: './password-confirm.component.html',
+  styleUrls: ['./password-confirm.component.scss']
+})
+export class PasswordConfirmComponent {
+
+}
