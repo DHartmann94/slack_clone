5609a0af02cfce392f3147614ae7c8441644cfde
MaximilianScheel <scheel.development@gmail.com>
2023-07-13 13:15:24 +0200

    Working on the profile card logic

--- src/app/header-bar/header-bar.component.ts
+++ src/app/header-bar/header-bar.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class HeaderBarComponent {
   isLogoutContainerOpen: boolean = false;
   isProfileCardOpen: boolean = false;
+  isEditProfileCardOpen: boolean = false;
+
 
   openLogoutContainer() {
     this.isLogoutContainerOpen = true;
@@ -24,6 +26,15 @@ export class HeaderBarComponent {
 
   closeUserProfile() {
     this.isProfileCardOpen = false;
+  }
+
+  openEditProfileCard() {
+    this.isEditProfileCardOpen = true;
+    this.isProfileCardOpen = false;
+  }
+
+  closeEditUserProfile() {
+    this.isEditProfileCardOpen = false;
   }
 }
 
