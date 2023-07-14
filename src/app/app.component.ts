edff3626da8985a47d62265d77e80b724d6de4a0
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-07-14 10:06:46 +0200

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slack_clone';
+  isLogoutContainerOpen = false;
+  isProfileCardOpen = false;
+  isEditProfileCardOpen = false;
 
   onlyLogo = false;
 
