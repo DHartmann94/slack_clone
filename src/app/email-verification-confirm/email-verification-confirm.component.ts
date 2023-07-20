9dba55ac5e3ba57a1965286526fd42121e9c69ed
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-07-20 11:34:22 +0200

    confirmEmail

--- src/app/email-verification-confirm/email-verification-confirm.component.ts
+++ src/app/email-verification-confirm/email-verification-confirm.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { applyActionCode, getAuth } from '@angular/fire/auth';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-email-verification-confirm',
   templateUrl: './email-verification-confirm.component.html',
   styleUrls: ['./email-verification-confirm.component.scss']
 })
-export class EmailVerificationConfirmComponent {
+export class EmailVerificationConfirmComponent implements OnInit {
+
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.confirmEmail();
+  }
+
+  async confirmEmail() {
+    const auth = getAuth();
+    const code = this.route.snapshot.queryParams['oobCode'];
+
+    await applyActionCode(auth, code)
+      .then(() => {
+        //how to update my backend based on the user's email
+        console.log('Email verify?');
+      })
+      .catch((error: any) => {
+        console.log('ERROR Email verify: ', error);
+      });
+  }
 
 }
