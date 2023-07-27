84bf064300cc3fe04984672d0fb3746be669885d
Adrian Herzberger <adrianherzberger94@gmail.com>
2023-07-27 16:42:52 +0200

    27.07.2023 Adrian

--- src/app/service-moduls/chat-shared.service.ts
+++ src/app/service-moduls/chat-shared.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ChatSharedService  {
-  private chatCreated: boolean = false;
+export class ChatSharedService implements CanActivate   {
 
-  setChatCreated(flag: boolean) {
-    this.chatCreated = flag;
-  }
-
-  getChatCreated() {
-    return this.chatCreated;
+  constructor(
+    private router: Router,
+  ){}
+  
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | UrlTree {
+    const boardRoute = this.router.config.find(route => route.path === 'board');
+    if (boardRoute && boardRoute.children) {
+      const hasChannelsComponent = boardRoute.children.some(route => route.path === 'channels');
+      const hasChatExtendComponent = boardRoute.children.some(route => route.path === 'chat-extended');
+      if (hasChannelsComponent && hasChatExtendComponent) {
+        return true;
+      }
+    }
+    return this.router.createUrlTree(['/']);
   }
 }
