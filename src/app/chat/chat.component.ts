a3bcc30796802802642271944a26df5f4dac79b2
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2023-08-07 08:16:56 +0200

    reactions

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -395,24 +395,9 @@ export class ChatComponent implements OnInit, OnChanges {
   }
 
 
-  // reactWithEmoji(emoji: string, index: number, messageId: string) {
-  //   let emojiArray = this.messageData[index].emojis;
-  //   if (this.existReaction(index)) {
-  //     let indexWithCurrentUser = emojiArray.findIndex((reaction: { [x: string]: string; }) => reaction['reaction-from'] === this.currentUser);
-  //     emojiArray[indexWithCurrentUser] = { 'emoji': emoji, 'reaction-from': this.currentUser };
-  //   } else {
-  //     emojiArray.push({ 'emoji': emoji, 'reaction-from': this.currentUser });
-  //   }
-  //   this.messageDataService.updateMessage(messageId, emojiArray);
-  //   this.emojisClickedBefore = undefined;
-  //   this.reactionListOpen = false;
-  // }
-
+  // TODO: --> Checken ob es funktioniert wenn verschiedene Nutzer reagieren. 
   reactWithEmoji(emoji: string, index: number, messageId: string) {
-
     let emojiArray = this.messageData[index].emojis;
-
-    
 
     //Wenn der CU bereits reagiert hat... funktioniert
     emojiArray.forEach((emoj: { [x: string]: any[]; }) => {
@@ -420,11 +405,6 @@ export class ChatComponent implements OnInit, OnChanges {
         const userIndex = emoj['reaction-from'].indexOf(this.userDataService.userName);
         emoj['reaction-from'].splice(userIndex, 1);
       }
-
-      // if (emoj['reaction-from'].length < 1) {
-      //   emojiArray.indexOf(emoj['emoji'])
-        
-      // }
     });
 
     //Wenn das Emoji bereits in dieser Nachricht existiert, dann wird nur in "reaction.." gepusht.
@@ -437,14 +417,14 @@ export class ChatComponent implements OnInit, OnChanges {
       emojiArray.push({ 'emoji': emoji, 'reaction-from': [this.userDataService.userName] });
     }
 
-
     // Wenn bei einem Emoji die ['reactions-from].length 0 ist, dann wird das Emoji aus dem Array gelöscht
-    //TODO!
-
+    //funktioniert!
+      let indexWithEmojiToDelete = emojiArray.findIndex((em: { [x: string]: string; }) => em['reaction-from'].length == 0);
+      if (indexWithEmojiToDelete != -1) {
+        emojiArray.splice(indexWithEmojiToDelete, 1);
+      }
 
     console.log('my Emoji Array', emojiArray);
-
-
 
     this.messageDataService.updateMessage(messageId, emojiArray);
     this.emojisClickedBefore = undefined;
