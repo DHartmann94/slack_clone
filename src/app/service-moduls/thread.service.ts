c2d089c536a31139c711dd2a0801859e6928d138
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-08-09 14:36:06 +0200

    a few tests: threads

--- src/app/service-moduls/thread.service.ts
+++ src/app/service-moduls/thread.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { DocumentData, Firestore, QuerySnapshot, collection, getDocs, query, addDoc, onSnapshot, where, updateDoc, doc, getDoc, setDoc, } from '@angular/fire/firestore';
-import { Observable, from, map, BehaviorSubject } from 'rxjs';
+import { Observable, from, map, BehaviorSubject, Subscription } from 'rxjs';
 import { UserDataInterface } from './user.service';
 import { MessageDataInterface } from './message.service';
 import { ChatDataInterface } from './chat.service';
@@ -86,4 +86,107 @@ export class ThreadDataService {
       return () => unsubscribe();
     });
   }
+
+  /*async openThread(messageId: string) { // TEST Daniel!!!
+    const docRef = doc(this.firestore, 'messages', messageId);
+    const docSnap = await getDoc(docRef);
+    const messageData = docSnap.data();
+    console.log(docSnap.id);
+
+    if (messageData) {
+      const threadDataSubscription: Subscription = this.getThreadData().subscribe(
+        async (threadData: ThreadDataInterface[]) => {
+          console.log(threadData);
+
+          const existThread = threadData.find(thread => thread['id'] === docSnap.id);
+          console.log(existThread);
+          if (existThread) {
+            //console.log('existThread',existThread);
+            
+            const threadData = {
+              id: existThread['id'],
+              messageText: existThread['messageText'],
+              time: existThread['time'],
+              thread: existThread['thread'],
+              emojis: existThread['emojis'],
+              channel: existThread['channel'],
+              sentById: existThread['sentById'],
+            };
+          } else {
+            const newThreadData = {
+
+              id: docSnap.id,
+              messageText: messageData['messageText'],
+              time: messageData['time'],
+              thread: messageData['thread'],
+              emojis: messageData['emojis'],
+              channel: messageData['channel'],
+              sentById: messageData['sentById'],
+            };
+            //console.log('New', newThreadData);
+            //const threadCollectionRef = collection(this.firestore, 'threads');
+            //await addDoc(threadCollectionRef, newThreadData);
+
+
+            //await setDoc(docRef, { thread: threadDocRef.id }, { merge: true });
+          }
+          threadDataSubscription.unsubscribe();
+        },
+        (error) => {
+          console.error('ERROR open thread:', error);
+          threadDataSubscription.unsubscribe();
+        }
+      );
+    }
+  }*/
+
+  /*getThreadData(): Observable<ThreadDataInterface[]> { // TEST Daniel!!!
+    const threadCollection = collection(this.firestore, 'threads');
+    const q = query(threadCollection);
+
+    return new Observable<ThreadDataInterface[]>((observer) => {
+      const unsubscribe = onSnapshot(q, async (querySnapshot) => {
+        const storedMessageData: ThreadDataInterface[] = [];
+
+        for (const doc of querySnapshot.docs) {
+          const data = doc.data();
+          const { messageText,time, thread, emojis, sentById, channel } = data;
+
+          try {
+            const userData = await this.userDataService.usersDataBackend(sentById);
+            let userName: string;
+            let userPicture: string;
+
+            if (userData !== null) {
+              userName = userData['name'];
+              userPicture = userData['picture'];
+            } else {
+              userName = 'Unknown User';
+              userPicture = '/assets/profile-pictures/unknown-user.png';
+            }
+
+            const threadData: ThreadDataInterface = {
+              id: doc.id,
+              messageText: messageText,
+              time: time,
+              thread: thread,
+              emojis: emojis,
+              channel: channel,
+              sentBy: userName,
+              picture: userPicture,
+              sentById: sentById,
+            };
+            storedMessageData.push(threadData);
+          } catch (error) {
+            console.log('ERROR retrieving thread data:', error);
+          }
+        }
+
+        this.threadDataSubject$.next(storedMessageData);
+        observer.next(storedMessageData);
+      });
+
+      return () => unsubscribe();
+    });
+  }*/
 }
