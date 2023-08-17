709a465603575b420b126f6cbe203f3349f9ce1c
Adrian Herzberger <adrianherzberger94@gmail.com>
2023-08-17 19:21:36 +0200

    17.08.2023 Adrian

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {ThreadDataInterface, ThreadDataService} from "../service-moduls/thread.service";
-<<<<<<< HEAD
+import { ThreadDataInterface, ThreadDataService} from "../service-moduls/thread.service";
 import { ThreadDirectDataInterface, ThreadDirectService } from '../service-moduls/thread-direct.service';
-=======
 import { DirectMessageToUserInterface, DirectMessageToUserService } from '../service-moduls/direct-message-to-user.service';
 import { ChatBehaviorService } from '../service-moduls/chat-behavior.service';
->>>>>>> 9eec681e233f83a9db65b68ad8d710fcfd573de9
 
 @Component({
   selector: 'app-board',
@@ -14,23 +11,13 @@ import { ChatBehaviorService } from '../service-moduls/chat-behavior.service';
 })
 
 export class BoardComponent implements OnInit {
-  
 
   constructor(
-<<<<<<< HEAD
-    public threadDataService: ThreadDataService,
-    public threadDirectDataService: ThreadDirectService,
-  ) { }
-=======
     public threadDataService: ThreadDataService, 
+    public threadDirectDataService: ThreadDirectService,
     public directMessageToUserService: DirectMessageToUserService,
     public chatBehaviorService: ChatBehaviorService,
-    
-    ) { }
-
-
-  
->>>>>>> 9eec681e233f83a9db65b68ad8d710fcfd573de9
+  ) { }
 
   ngOnInit(): void {
     
