c44d5c8011b44ea46aeb6cc4bfe1c6ee3de657aa
AdrianHerzberger <119050739+AdrianHerzberger@users.noreply.github.com>
2023-08-18 11:27:16 +0200

    Add files via upload

---
+++ src/app/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCRzcpWIXdmu9KVLcbkRKt02yYYzmDJNrE",
+        authDomain: "slag-clone.firebaseapp.com",
+        databaseURL: "https://slag-clone-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "slag-clone",
+        storageBucket: "slag-clone.appspot.com",
+        messagingSenderId: "219350555695",
+        appId: "1:219350555695:web:f4a0e3cf49d257cc963ada"
+    }
+}; 
